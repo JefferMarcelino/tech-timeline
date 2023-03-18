bd2f66b282dc870e75f5b72fd26aab5dc066cfd3
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-03-18 20:52:55 +0200

    feat: create event timeline component

---
+++ src/components/EventTimeLine.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { motion } from "framer-motion";
+
+interface EventTimeLineType {
+  event: String
+}
+
+const EventTimeLine:React.FC<EventTimeLineType> = ({ event }) => {
+  return (
+    <div className="pb-5 w-full border-l-2 border-gray-500 pl-8 relative before:absolute before:w-4 before:h-4 before:rounded-full before:-left-[9px] before:top-[10px] before:bg-gray-500">
+      <motion.p 
+        //drag
+        //dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        initial={{ y: 300 }}
+        whileInView={{ y: 0, transition: { type: "spring", bounce: 0.4, duration: 0.8 } }}
+        viewport={{ once: true, amount: 0.8 }}
+        className="marker shadow bg-white rounded p-2 relative before:absolute
+      before:w-5 before:h-5 before:bg-white before:-left-2 before:rotate-45 before:z-0">
+        <span className="relative z-10 dark:text-[#242E4C]">{ event }</span>
+      </motion.p>
+    </div>
+  )
+}
+
+export default EventTimeLine;
