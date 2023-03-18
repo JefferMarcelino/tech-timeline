dd6f4ea7c47a0636cda46057543bdfaa271e0e89
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-03-18 20:53:27 +0200

    feat: add events data

---
+++ src/database/data.ts
@@ -0,0 +1,279 @@
+export interface Event {
+  year: Number
+  events: String[]
+}
+
+const keyEvents: Event[] = [
+  {
+    "year": 1709,
+    "events": [
+      "First steam-powered machine is patented by Thomas Newcomen."
+    ],
+  },
+  {
+    "year": 1712,
+    "events": [
+      "Thomas Newcomen and John Calley develop an improved steam engine for pumping water out of mines."
+    ],
+  },
+  {
+    "year": 1733,
+    "events": [
+      "John Kay invents the flying shuttle, revolutionizing the weaving industry."
+    ],
+  },
+  {
+    "year": 1746,
+    "events": [
+      "Benjamin Franklin invents the lightning rod.",
+      "The Leyden jar, an early form of capacitor, is invented by Pieter van Musschenbroek.",
+      "The first recorded instance of artificially induced rain is carried out by scientist Johann Wilhelm Ritter."
+    ],
+  },
+  {
+    "year": 1764,
+    "events": [
+      "James Hargreaves invents the spinning jenny, enabling faster textile production."
+    ],
+  },
+  {
+    "year": 1765,
+    "events": [
+      "James Watt develops an improved steam engine, leading to further advancements in industry and transportation."
+    ],
+  },
+  {
+    "year": 1775,
+    "events": [
+      "Johan Gadolin discovers the element yttrium, leading to the development of new technologies such as color television and fluorescent lights."
+    ],
+  },
+  {
+    "year": 1783,
+    "events": [
+      "The Montgolfier brothers invent the hot air balloon.",
+      "James Rumsey demonstrates the first successful steamboat."
+    ],
+  },
+  {
+    "year": 1799,
+    "events": [
+      "Alessandro Volta invents the first electric battery, paving the way for the development of modern electronics."
+    ],
+  },
+  {
+    "year": 1807,
+    "events": [
+      "Robert Fulton demonstrates the first commercially successful steamboat, the North River Steamboat."
+    ],
+  },
+  {
+    "year": 1822,
+    "events": [
+      "Charles Babbage conceives the concept of a programmable computer."
+    ],
+  },
+  {
+    "year": 1831,
+    "events": [
+      "Michael Faraday discovers electromagnetic induction, leading to the development of electric motors and generators."
+    ],
+  },
+  {
+    "year": 1837,
+    "events": [
+      "Samuel Morse invents the telegraph."
+    ],
+  },
+  {
+    "year": 1859,
+    "events": [
+      "Edwin Drake drills the first successful oil well in Pennsylvania."
+    ],
+  },
+  {
+    "year": 1866,
+    "events": [
+      "Transatlantic telegraph cable is laid, enabling instantaneous communication between Europe and America."
+    ],
+  },
+  {
+    "year": 1876,
+    "events": [
+      "Alexander Graham Bell invents the telephone."
+    ],
+  },
+  {
+    "year": 1886,
+    "events": [
+      "Karl Benz invents the first gasoline-powered automobile."
+    ],
+  },
+  {
+    "year": 1895,
+    "events": [
+      "Wilhelm Conrad Roentgen discovers X-rays."
+    ],
+  },
+  {
+    "year": 1901,
+    "events": [
+      "Guglielmo Marconi sends the first transatlantic wireless transmission."
+    ],
+  },
+  {
+    "year": 1903,
+    "events": [
+      "Wilbur and Orville Wright achieve the first powered flight."
+    ],
+  },
+  {
+    "year": 1927,
+    "events": [
+      "Philips develops the first television."
+    ],
+  },
+  {
+    "year": 1947,
+    "events": [
+      "William Shockley, John Bardeen, and Walter Brattain invent the transistor, leading to the development of modern electronics."
+    ],
+  },
+  {
+    "year": 1957,
+    "events": [
+      "The Soviet Union launches the first artificial satellite, Sputnik 1."
+    ],
+  },
+  {
+    "year": 1969,
+    "events": [
+      "Neil Armstrong becomes the first human to set foot on the moon."
+    ],
+  },
+  {
+    "year": 1971,
+    "events": [
+      "Intel introduces the first microprocessor, the Intel 4004."
+    ],
+  },
+  {
+    "year": 1981,
+    "events": [
+      "IBM launches the first personal computer, the IBM PC."
+    ],
+  },
+  {
+    "year": 1989,
+    "events": [
+      "Tim Berners-Lee invents the World Wide Web."
+    ],
+  },
+  {
+    "year": 1997,
+    "events": [
+      "The first successful mammal cloning, Dolly the sheep, is announced."
+    ],
+  },
+  {
+    "year": 2001,
+    "events": [
+      "Apple introduces the first iPod.",
+      "Wikipedia, a free online encyclopedia, is launched.",
+      "Microsoft releases Windows XP."
+    ],
+  },
+  {
+    "year": 2003,
+    "events": [
+      "Spacecraft Mars Exploration Rover lands on Mars."
+    ],
+  },
+  {
+    "year": 2004,
+    "events": [
+      "Facebook is launched."
+    ],
+  },
+  {
+    "year": 2005,
+    "events": [
+      "YouTube is launched."
+    ],
+  },
+  {
+    "year": 2007,
+    "events": [
+      "Apple releases the first iPhone."
+    ],
+  },
+  {
+    "year": 2008,
+    "events": [
+      "Google releases the first version of its mobile operating system, Android."
+    ],
+  },
+  {
+    "year": 2010,
+    "events": [
+      "Apple releases the first iPad.",
+      "Tesla Motors releases its first electric car, the Tesla Roadster."
+    ],
+  },
+  {
+    "year": 2011,
+    "events": [
+      "IBM's Watson defeats human champions in the game show Jeopardy!"
+    ],
+  },
+  {
+    "year": 2012,
+    "events": [
+      "SpaceX becomes the first privately-funded company to send a spacecraft to the International Space Station."
+    ],
+  },
+  {
+    "year": 2014,
+    "events": [
+      "Apple releases the first Apple Watch."
+    ],
+  },
+  {
+    "year": 2015,
+    "events": [
+      "NASA's New Horizons spacecraft flies by Pluto."
+    ],
+  },
+  {
+    "year": 2016,
+    "events": [
+      "AlphaGo, a computer program developed by Google, defeats a world champion in the board game Go."
+    ],
+  },
+  {
+    "year": 2017,
+    "events": [
+      "Tesla releases the Model 3, its first mass-market electric car."
+    ],
+  },
+  {
+    "year": 2019,
+    "events": [
+      "The first image of a black hole is captured by the Event Horizon Telescope collaboration."
+    ],
+  },
+  {
+    "year": 2020,
+    "events": [
+      "The COVID-19 pandemic drives rapid development and deployment of telemedicine and remote work technologies."
+    ],
+  },
+  {
+    "year": 2021,
+    "events": [
+      "SpaceX launches the first all-civilian crew into orbit aboard the Dragon spacecraft."
+    ],
+  }
+]
+
+export { keyEvents }
